09bbea1611fa67f0f4d2a61eae995c80023d2bb9
Jimena Sejas <sejasjimena11@gmail.com>
2022-08-30 00:01:10 -0300

    Contador

--- my-app/src/components/ItemListContainer.js
+++
@@ -1,5 +0,0 @@
-const ItemListContainer = () => {
-  return <div>Nuestro catálogo</div>;
-};
-
-export default ItemListContainer;
---
+++ my-app/src/components/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+const ItemListContainer = () => {
+  function onAdd(count) {
+    console.log(`Se han seleccionado ${count} productos`);
+  }
+  return (
+    <div>
+      <Counter stock={5} onAdd={onAdd} min={1} sum={1} res={1} />
+    </div>
+  );
+};
+
+export default ItemListContainer;
